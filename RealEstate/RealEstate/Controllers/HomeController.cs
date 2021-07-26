e8da596feb00da7e7678e40085b40893c4fd0e39
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-26 11:39:37 +0200

    Setting Up an MVC App to Connect to Mongo
    Setting Up an MVC App to Connect to Mongo

--- RealEstate/RealEstate/Controllers/HomeController.cs
+++ RealEstate/RealEstate/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using RealEstate.Models;
 using System;
 using System.Collections.Generic;
@@ -12,15 +14,25 @@ namespace RealEstate.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public MongoDatabase Database;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
+
+            var client = new MongoClient(configuration["ConnectionStrings:RealEstateConnectionString"]);
+            var server = client.GetServer();
+            Database = server.GetDatabase(configuration["RealEstateDatabaseName"]);
         }
 
         public IActionResult Index()
         {
-            return View();
+            //return View();
+
+            Database.GetStats();
+            return Json(Database.Server.BuildInfo);
         }
 
         public IActionResult Privacy()
