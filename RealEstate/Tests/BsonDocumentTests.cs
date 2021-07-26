65734b49ac3eed88fd81ed71a40dd9c51602a29f
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-26 13:52:43 +0200

    Demo: Creating Documents with BsonDocument
    Demo: Creating Documents with BsonDocument

---
+++ RealEstate/Tests/BsonDocumentTests.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    public class BsonDocumentTests
+    {
+        public BsonDocumentTests()
+        {
+            JsonWriterSettings.Defaults.Indent = true;
+        }
+
+        [Test]
+        public void EmptyDocument()
+        {
+            var document = new BsonDocument();
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        [Test]
+        public void AddingElements()
+        {
+            //var person = new BsonDocument();
+            var person = new BsonDocument
+            {
+                {"age", new BsonInt32(54) },
+                {"IsAlive", true }
+            };
+            person.Add("firstName", new BsonString("Gabriel"));
+
+            Console.WriteLine(person);
+        }
+
+        [Test]
+        public void AddingArrays()
+        {
+            var person = new BsonDocument();
+            person.Add("adress", new BsonArray(new[] { "101 Some Road", "Unit 501" }));
+
+            Console.WriteLine(person);
+        }
+
+        [Test]
+        public void EmbeddedDocument()
+        {
+            var person = new BsonDocument
+            {
+                {"contact", new BsonDocument
+                {
+                    {"phone", "123-456-789" },
+                    {"email","email@email.com" }
+                }
+                }
+            };
+
+            Console.WriteLine(person);
+        }
+    }
+}
